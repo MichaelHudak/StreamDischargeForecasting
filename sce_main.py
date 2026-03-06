3a666dba358608a95938f20e402e22ebc73dc835
MichaelHudak <hudakmichael5@gmail.com>
2026-03-06 19:24:28 +0000

    messing around with csv links

--- sce_main.py
+++ sce_main.py
@@ -75,20 +75,18 @@ loc_stat_ids = {
 
 data_source_dict = {
     'A' : {'stream': 'USGS-1571184', 'gw': 'USGS-400209077183301',
-               'weather' : r"weather_data\A_Biglerville_weather.csv"},
+               'weather' : r"\weather_data\A_Biglerville_weather.csv"},
     'B' : {'stream': 'USGS-1567000', 'gw': 'USGS-402735077100901',
-               'weather' : r"weather_data\B_Lewistown_weather.csv"},
+               'weather' : r"\weather_data\B_Lewistown_weather.csv"},
     'C' : {'stream': 'USGS-1550000', 'gw': 'USGS-412427076594401',
-               'weather' : r"weather_data\C_Williamsport_weather.csv"},
+               'weather' : r"\weather_data\C_Williamsport_weather.csv"},
     'D' : {'stream': 'USGS-1526500', 'gw': 'USGS-420710077052101',
                'weather' : r"weather_data\D_Corning_weather.csv"},
     'E' : {'stream': 'USGS-1514000', 'gw': 'USGS-420815076155501',
-               'weather' : r"weather_data\E_Binghamton_weather.csv"}
+               'weather' : r"\weather_data\E_Binghamton_weather.csv"}
 }
 
-
 # In[ ]:
-
 
 letter = "A"
 stream_df = get_stream_data(data_source_dict[letter]["stream"])
